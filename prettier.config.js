2008b58c82461cd69f152b67770f14271175ed0b
Juan Cassiano <juancassiano@hotmail.com>
2020-06-30 18:02:27 -0300

    Style config

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true, // Mantém aspas simples
+  trailingComa: "all", // Termina a linha de código com vigula
+  arrowParens: "avoid", // Não coloca parenteses por volta das arrow functions com um parâmetro
+};
