a33214f86cf6aa2daa3f9ec27d6da592747561f9
Juan Cassiano <juancassiano@hotmail.com>
2020-07-06 17:08:41 -0300

    Form input

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Image, View, ScrollView, KeyboardAvoidingView, Platform  } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import { Container, Title, ForgotPassword, ForgotPasswordText,CreateAccountButton, CreateAccountButtonText } from './styles';
 import logoImg from '../../assets/logo.png';
 
+
 const SignIn: React.FC = () => {
   const navigation = useNavigation();
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSignIn = useCallback((data: object) => {
+    console.log(data);
+  },[]);
 
   return (
     <>
@@ -29,11 +37,15 @@ const SignIn: React.FC = () => {
               <Title>Faça seu Logon</Title>
             </View>
 
-            <Input name="email" icon="mail" placeholder="E-mail"/>
+            <Form ref={formRef} onSubmit={handleSignIn}>
+              <Input name="email" icon="mail" placeholder="E-mail"/>
 
-            <Input name="password" icon="lock" placeholder="Senha"/>
+              <Input name="password" icon="lock" placeholder="Senha"/>
 
-            <Button onPress={() => {console.log('Foi')}}>Entrar</Button>
+              <Button onPress={() => { formRef.current?.submitForm()}}>
+                Entrar
+              </Button>
+            </Form>
 
             <ForgotPassword onPress={() => {}}>
               <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
